459e8c2de63d8f5e0eb04b93ab5891c7b06d00f7
mauriciomac92 <mauricio.macias@csu.fullerton.edu>
2019-03-21 23:32:50 -0700

    made a spawn script to spawn any object to the game

---
+++ EventHorizon/Assets/Scripts/Spawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject spawnee;
+    public bool stopSpawing = false;
+    public float spawnTime;
+    public float spawnDelay;
+
+    void Start() {
+        InvokeRepeating("SpawnObject", spawnTime, spawnDelay);
+    }
+
+    public void SpawnObject() {
+        Instantiate(spawnee, transform.position, transform.rotation);
+        if (stopSpawing) {
+            CancelInvoke("SpawnObject");
+        }
+    }
+}
