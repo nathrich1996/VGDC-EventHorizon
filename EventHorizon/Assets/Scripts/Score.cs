5acfe25d1c8a9d0029634efa2bf79bf6f0acd332
Nathaniel <nate.richards96@gmail.com>
2019-03-27 02:22:36 -0700

    N: Added Score to Screen with Fonts
    Added Neutra Text Fonts as Placeholder.

---
+++ EventHorizon/Assets/Scripts/Score.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Text score_text;
+    private int score;
+    private int multiplier;
+    private float timer;
+    private float score_interval = 0.20f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        multiplier = 10;
+        timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer > score_interval) //add score by 10 every three seconds
+        {
+            score += multiplier;
+            score_text.text = score.ToString();
+            timer = 0;
+        }
+        
+    }
+}
