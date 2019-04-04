a286e0c0a82c8f1cd50b5d66a6b3cdbb419a48fd
Nathaniel <nate.richards96@gmail.com>
2019-04-04 00:00:59 -0700

    N: Added Obstacle Movement Scripts

---
+++ EventHorizon/Assets/Scripts/Obstacle.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    Rigidbody rb;
+    private float obsSpeed = 250.0f; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.AddForce(0,0,-obsSpeed * Time.deltaTime);
+    }
+    void CheckOffScreen()
+    {
+        if (transform.position.z < -90.0f)
+        {
+            Destroy(GetComponent<Rigidbody>());
+        }
+    }
+}
