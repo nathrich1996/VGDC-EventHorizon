0c91425425ebf5b839aad8bf8acdaaa3053da971
Nathaniel <nate.richards96@gmail.com>
2019-04-12 13:57:28 -0700

    N: Improved Spawner with Prefabs

--- EventHorizon/Assets/Scripts/Spawner.cs
+++ EventHorizon/Assets/Scripts/Spawner.cs
@@ -5,23 +5,23 @@ using UnityEngine;
 public class Spawner : MonoBehaviour
 {
     //public GameOb spawnee;
-    public Obstacle obstacle;
-    public FuelGem fuelGem;
+    public GameObject obstacle;
+    public GameObject fuelGem;
     public bool stopSpawing = false;
     float spawnTime =2.0f;
     float spawnDelay = 2.0f;
     
     Vector3 spawnPoint; //point 
     string chosenSpawnObject;
-    string[] spawnObjects =  { "obstacle"}; //holds all possible items that can be spawned
+    string[] spawnObjects =  { "obstacle","gem"}; //holds all possible items that can be spawned
     float[] xSpawnPoints = { -20f, -10f, 0f, 10f, 20f };
     int randSpawnPoint; //index of randomly chosen spawn point
     int randSpawnObject; // index of randomy chosen spawn object
 
     void Start() {
         spawnPoint = new Vector3(0,.6f,-15f);
-        fuelGem = GameObject.FindGameObjectWithTag("Fuel").GetComponent<FuelGem>();
-        obstacle = GameObject.FindGameObjectWithTag("Obstacle").GetComponent<Obstacle>();
+        //fuelGem = GameObject.FindGameObjectWithTag("Fuel").GetComponent<FuelGem>();
+        //obstacle = GameObject.FindGameObjectWithTag("Obstacle").GetComponent<Obstacle>();
         InvokeRepeating("SpawnObject", spawnTime, spawnDelay);
     }
     public void GetSpawnPointandObject()
