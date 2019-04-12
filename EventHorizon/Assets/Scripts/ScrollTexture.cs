d4618ddc626e3726f57f9805dafd4835a465b3ec
Noah C <nschristensen90@gmail.com>
2019-04-12 13:03:28 -0700

    NC: Added Bridge Animation and Fixed Rotation

---
+++ EventHorizon/Assets/Scripts/ScrollTexture.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollTexture : MonoBehaviour
+{
+	public float ScrollY = 0.5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float OffsetY = Time.time * ScrollY;
+		GetComponent<Renderer>().material.mainTextureOffset = new Vector2 (0, OffsetY);
+    }
+}
