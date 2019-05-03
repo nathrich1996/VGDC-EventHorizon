454f481e10df3e9f90208df8604c37b138862495
Noah Christensen <theultimatepikachu@gmail.com>
2019-05-02 18:00:49 -0700

    NC: Added side to side animations
    are disabled

--- EventHorizon/Assets/Scripts/PlayerController.cs
+++ EventHorizon/Assets/Scripts/PlayerController.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
      Rigidbody rb;
+
+    public Animator anim;
+
     public float laneChangeSpeed = 0f; //enough to move from one lane to the next 50000
     //public const float LANE_DIST = 3500.0f;
     // Start is called before the first frame p
@@ -27,12 +30,12 @@ public class PlayerController : MonoBehaviour
         Debug.Log("Game Started!");
         rb = GetComponent<Rigidbody>();
         position = Vector2.zero;
+        anim.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
         //Finding Input
         if (Input.GetKeyDown("a")) //move left
         {
@@ -52,63 +55,18 @@ public class PlayerController : MonoBehaviour
            
             Debug.Log("D press");
         }
-
-        //if (!Input.anyKeyDown)    
-        //{
-        //    movingRight = false;
-        //    movingLeft = false;
-        //}
-
-        //switch (lane)
-        //{
-        //    case 1: //Leftmost end of the map
-        //        position -= Vector3.right * LANE_DIST;
-        //        Debug.Log("In Lane 1");
-        //        break;
-        //    case 2:
-        //        if (movingRight)
-        //            position += Vector3.right * LANE_DIST;
-        //        else if (movingLeft)
-        //            position -= Vector3.right * LANE_DIST;
-        //        Debug.Log("In Lane 2");
-        //        break;
-        //    case 3:
-        //        if (movingRight)
-        //            position += Vector3.right * LANE_DIST;
-        //        else if (movingLeft)
-        //            position -= Vector3.right * LANE_DIST;
-        //        Debug.Log("In Lane 3");
-        //        break;
-        //    case 4:
-        //        if (movingRight)
-        //            position += Vector3.right * LANE_DIST;
-        //        else if (movingLeft)
-        //            position -= Vector3.right * LANE_DIST;
-        //        Debug.Log("In Lane 4");
-        //        break;
-        //    case 5:
-        //        position += Vector3.right * LANE_DIST;
-        //        Debug.Log("In Lane 5");
-        //        break;
-
-        //}
-        //Vector3 moveVector = Vector3.zero;
-        //moveVector.x = (position - transform.position).normalized.x * laneChangeSpeed;
-        ////moveVector.y = 0.6f;
-        ////moveVector.z = -42.3f;
-        ////rb.MovePosition(moveVector * Time.deltaTime);
-        ////if (movingLeft || movingRight)
-        ////    rb.position += moveVector;
        
         if (movingRight)
         {
             if (transform.position.x < lanePositions[lane - 1]) //move right
             {
+               // anim.SetTrigger("MoveRight");
                 rb.MovePosition(new Vector2(transform.position.x + (laneChangeSpeed * Time.deltaTime), transform.position.y));
             }
             if (transform.position.x > lanePositions[lane - 1]) //don't go too far
             {
                 rb.MovePosition(new Vector2(lanePositions[lane - 1], transform.position.y));
+              //  anim.ResetTrigger("MoveRight");
             }
 
         }
@@ -116,14 +74,18 @@ public class PlayerController : MonoBehaviour
         {
             if (transform.position.x > lanePositions[lane - 1]) //move left
             {
+               // anim.SetTrigger("MoveLeft");
                 rb.MovePosition(new Vector2(transform.position.x - (laneChangeSpeed * Time.deltaTime), transform.position.y));
             }
             if (transform.position.x < lanePositions[lane - 1]) //don't go too far
             {
                 rb.MovePosition(new Vector2(lanePositions[lane - 1], transform.position.y));
+               // anim.ResetTrigger("MoveLeft");
             }
         }
-        
+        //anim.ResetTrigger("MoveRight");
+       // anim.ResetTrigger("MoveLeft");
+
     }
 
     void MoveLane(bool right)
@@ -135,4 +97,6 @@ public class PlayerController : MonoBehaviour
         if (lane == 0)
             ++lane;
     }
+
+ 
 }
