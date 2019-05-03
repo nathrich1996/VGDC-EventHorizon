e77c1174db54513e7a09ed7a03cbae0ff06c1b17
Nathaniel <nate.richards96@gmail.com>
2019-05-03 13:21:50 -0700

    N: Shrinked Back Button and Made Background Spawner

---
+++ EventHorizon/Assets/Scripts/BackgroundSpawner.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundSpawner : MonoBehaviour
+{
+    public GameObject spawnee;
+    public GameObject[] obstacles;
+    public bool stopSpawing = false;
+    public float spawnTime;
+    public float spawnDelay;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnObject", spawnTime, spawnDelay);
+    }
+
+    public void SpawnObject()
+    {
+        int chosenSpawnee = Random.Range(0, obstacles.Length);
+        spawnee = obstacles[chosenSpawnee];
+        Instantiate(spawnee, transform.position, transform.rotation);
+        if (stopSpawing)
+        {
+            CancelInvoke("SpawnObject");
+        }
+    }
+}
