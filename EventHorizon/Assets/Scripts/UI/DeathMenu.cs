9d454f99ad046f99cd16dc752bede6836fc8d274
Noah Christensen <theultimatepikachu@gmail.com>
2019-05-03 11:08:52 -0700

    Removed gratuitous code.

--- EventHorizon/Assets/Scripts/UI/DeathMenu.cs
+++ EventHorizon/Assets/Scripts/UI/DeathMenu.cs
@@ -7,14 +7,17 @@ public class DeathMenu : MonoBehaviour
 {
     // Start is called before the first frame update
     public bool deathScreenActive = false;
+    MusicControl mControl;
     void Start()
     {
         gameObject.SetActive(false);
+        mControl = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<MusicControl>();
     }
 
     // Update is called once per frame
     public void ToggleDeathMenu()
     {
+        mControl.DeathAudio();
         deathScreenActive = true;
         gameObject.SetActive(true);
     }
